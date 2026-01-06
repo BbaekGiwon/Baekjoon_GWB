182d8b7ae142432e649e05a9951e494219d9ddeb
BbaekGiwon <01gwbaek@naver.com>
2026-01-06 23:38:17 +0900

    2026.01.06

---
+++ baekjoon-9019.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int T;	
+	cin >> T;
+	
+	for(int i=0; i<T; i++) {
+		int input, result;
+		cin >> input >> result;
+		
+	}
+	
+	
+	
+	return 0;
+}
